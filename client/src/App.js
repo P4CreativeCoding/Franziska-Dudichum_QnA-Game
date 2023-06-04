338d2a1a3ea0a7debf3d36c083f4a0c726d14f74
franzidu <stfrdudi@stud.h-da.de>
2023-06-04 23:22:48 +0200

    added data, filled client-webpage

--- client/src/App.js
+++ client/src/App.js
@@ -16,13 +16,35 @@ useEffect(() => {
 
   return ( 
     <div>
-      {(typeof backendData.users === 'undefined')? (
-        <p>Loading...</p>
-      ):(
-        backendData.users.map(( user, i ) => (
-          <p key={i}>{user}</p>
-        ))
-      )}
+      <header>
+        <h2>Q & A Game</h2>
+        <h4>Wassup and welcome to my qna game</h4>
+        <p>you will get a random question, where you can choose between 4 diffrent answers, which are chosen randomly</p>
+      </header>  
+      <div className='qna-section'>
+        <h3>Your question:</h3>
+        <div>
+          {(typeof backendData.users === 'undefined')? (
+            <p>Loading...</p>
+          ):(
+            backendData.users.map(( user, i ) => (
+              <p key={i}>{user}</p>
+            ))
+          )}
+        </div>
+
+        <h3>Your answers:</h3>
+
+        <div>
+          {(typeof backendData.users === 'undefined')? (
+            <p>Loading...</p>
+          ):(
+            backendData.users.map(( user, i ) => (
+              <p key={i}>{user}</p>
+            ))
+          )}
+        </div>
+      </div>
     </div>
   )
 }
--- server/server.js
+++ server/server.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 
 app.get("/api" , (req, res ) => {
-    res.json({ "users": [" user1", "user2", "user3 "] })
+    res.json({ "questions": [" question1", "question2", "question3 "] })
 })  
+
+app.get("/api" , (req, res ) => {
+    res.json({ "answers": [" answer1", "answer2", "answer3 "] })
+}) 
 
 app.listen(4000, () => { console.log("server started on port 4000") })
