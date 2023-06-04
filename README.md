9fc008a4b07eb0792f669a6d41d86ae1513c53c3
franzidu <stfrdudi@stud.h-da.de>
2023-06-04 20:35:41 +0200

    complete new code
    added vercel server deploy and react app

--- README.md
+++
@@ -1,7 +0,0 @@
-# server-deploy
-
-Erreichbar unter:
-https://server-deploy-steel.vercel.app/api/product
-
-Verwendetes Tutorial:
-https://dev.to/hte305/deploy-express-js-app-to-vercel-38jb
--- server.js
+++ server.js
@@ -1,40 +1,11 @@
-/*
 const express = require('express');
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-
-io.on('connection', socket => {
-  console.log('A user connected.');
-  socket.on('join', username => {
-    console.log(`${username} joined the game.`);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('A user disconnected.');
-  });
-});
-
 const port = process.env.PORT || 3000;
-http.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+
+app.get('/', (req, res) => {
+  res.send('test');
 });
-*/
 
-const express = require("express");
-const http = require("http");
-const socketIo = require("socket.io");
-
-const app = express();
-
-const server = http.createServer(app);
-const io = socketIo(server);
-
-/*
-const connectSocket = io(process.env.REACT_APP_SERVER, {
-    query: {
-      username: "user1",
-      password: "password1"
-    }
-  }); 
-  */
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
