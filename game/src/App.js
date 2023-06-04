a58731e962ad5f680b7169e79a8201520be7d55a
franzidu <stfrdudi@stud.h-da.de>
2023-06-04 20:42:59 +0200

    new code

---
+++ game/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <body>
+        <header>
+          <p>moin</p>
+        </header>
+        <div>
+          <p>was geht</p>
+        </div>
+        <footer>
+          <p>tschö mit ö</p>
+        </footer>
+      </body>
+    </div>
+  );
+}
+
+export default App;
