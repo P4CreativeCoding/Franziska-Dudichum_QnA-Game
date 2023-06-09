48b52ef796b349505829bf8f24d4700b2c15002d
franziska.dudichum <franzi-du@web.de>
2023-06-09 23:35:11 +0200

    login authentication added

--- client/src/App.js
+++ client/src/App.js
@@ -1,88 +1,37 @@
-import React, { useEffect, useState } from "react";
-import questionData from "./components/data/questions.json";
-import answerData from "./components/data/answers.json";
+import React, { useState } from "react";
+import Display from "./components/display/display.jsx";
 
 function App() {
-  //--------------------------------CONST---------------------------------------------//
-  //normal backend
-  const [backendData, setBackendData] = useState([{}]);
-  //Random question
-  const [randomQuestion, setRandomQuestion] = useState(null);
-  //Answers
-  const [data, setData] = useState(null);
-  
-  //------------------------------FUNCTIONS-----------------------------------------------//
-  //Random question
-  function getRandomQuestion() {
-    const randomIndex = Math.floor(Math.random() * questionData.length);
-    const randomQuestion = questionData[randomIndex];
-    setRandomQuestion(randomQuestion);
-  }
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [password, setPassword] = useState("");
 
-  //Answer Select
-  function handleButtonClick(answer) {
-    console.log(answer); // Example: Log the clicked item to the console
-    // Add your desired logic here
-  }
-  //---------------------------------USE-EFFECT--------------------------------------------//
+  const handleLogin = () => {
+    if (password === "12") {
+      setIsLoggedIn(true);
+    } else {
+      alert("Access denied! Incorrect password.");
+    }
+  };
 
-  //normal backend
-  useEffect(() => {
-    fetch("/api ")
-      .then((response) => response.json())
-      .then((data) => {
-        setBackendData(data);
-      });
-  }, []);
-
-  //Random question
-  useEffect(() => {
-    getRandomQuestion();
-  }, []);
-
-  //---------------------------------RENDER--------------------------------------------//
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
 
   return (
     <div>
-      <header>
-        <h2>Q & A Game</h2>
-        <h4>Wassup and welcome to my qna game</h4>
-        <p>
-          you will get a random question, where you can choose between 4
-          different answers, which are chosen randomly
-        </p>
-      </header>
-
-      <div className="qna-section">
-        <h3>Your question:</h3>
+      {!isLoggedIn ? (
         <div>
-          {randomQuestion ? (
-            <div>
-              {/* <p>Question ID: {randomQuestion.id}</p> */}
-              <p>Question: {randomQuestion.text}</p>
-            </div>
-          ) : (
-            <p>Loading...</p>
-          )}
+          <h2>Login</h2>
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <button onClick={handleLogin}>Login</button>
         </div>
-
-        <h3>Your answers:</h3>
-
-
-        {answerData.map((answer) => (
-      <button key={answer.id} onClick={() => handleButtonClick(answer)}>
-        {answer.text}
-      </button>
-    ))}
-        {/* <div>
-          <ul>
-          {answerData.map((item) => (
-            <button key={item.id}>{item.text}</button>
-          ))}
-          </ul>
-        </div>
-         */}
-      </div>
+      ) : (
+        <div>{<Display />}</div>
+      )}
     </div>
   );
 }
---
+++ client/src/components/display/display.jsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import questionData from "../data/questions.json";
+import answerData from "../data/answers.json";
+
+function Display() {
+  const [backendData, setBackendData] = useState([{}]);
+  const [randomQuestion, setRandomQuestion] = useState(null);
+  const [data, setData] = useState(null);
+
+  function getRandomQuestion() {
+    const randomIndex = Math.floor(Math.random() * questionData.length);
+    const randomQuestion = questionData[randomIndex];
+    setRandomQuestion(randomQuestion);
+  }
+
+  function handleButtonClick(answer) {
+    console.log(answer);
+    // Add your desired logic here
+  }
+
+  useEffect(() => {
+    fetch("/api")
+      .then((response) => response.json())
+      .then((data) => {
+        setBackendData(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    getRandomQuestion();
+  }, []);
+
+  return (
+    <div>
+      <header>
+        <h2>Q & A Game</h2>
+        <h4>Wassup and welcome to my qna game</h4>
+        <p>
+          you will get a random question, where you can choose between 4
+          different answers, which are chosen randomly
+        </p>
+      </header>
+      <div className="qna-section">
+        <h3>Your question:</h3>
+        <div>
+          {randomQuestion ? (
+            <div>
+              {/* <p>Question ID: {randomQuestion.id}</p> */}
+              <p>Question: {randomQuestion.text}</p>
+            </div>
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
+
+        <h3>Your answers:</h3>
+
+        {answerData.map((answer) => (
+          <button key={answer.id} onClick={() => handleButtonClick(answer)}>
+            {answer.text}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Display;
